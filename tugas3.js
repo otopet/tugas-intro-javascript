c73e1db56149fc29e50253c89607d3e843210fbd
otopet <77085799+otopet@users.noreply.github.com>
2021-05-25 22:00:26 +0700

    Create tugas3.js

---
+++ tugas3.js
@@ -0,0 +1,26 @@
+// gunakan logical operator dan comparison operator
+// silahkan isi bebas nilai dari variabel dibawah ini
+let a = ___;
+let b = ___;
+
+/* AND operator */
+console.log(a < 15 && b > 10); // (true && true) -> true
+console.log(___); // (false && true) -> false
+
+/* OR operator */
+console.log(___); // (true || true) -> true
+console.log(___); // (false || true) -> true
+
+/* NOT operator */
+console.log(___); // !(true) -> false
+console.log(___); // !(true && true) -> !(true) -> false
+
+
+/* output yang dihasilkan
+true
+false
+true
+true
+false
+false
+*/
