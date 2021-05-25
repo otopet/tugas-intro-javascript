377b6a6392a8d4358968485e6bcd4fa76d5e9fda
otopet <77085799+otopet@users.noreply.github.com>
2021-05-25 21:54:29 +0700

    Create tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+// gunakan comparison operator
+const nilai1 = ___; // silahkan isi sendiri nilai variabelnya
+const nilai2 = ___;
+
+console.log(___); 
+console.log(___);
+
+
+/* output yang dihasilkan harus bernilai boolean sebagai berikut:
+true
+false
+*/
