f532adf5911ef2654bb1b8c7e3b715bf35205136
OvanAlviansyah <ovanalviansyah217@gmail.com>
2021-05-26 16:16:37 +0700

    Upload tugas PWD JavaScript

---
+++ 19010014_OvanFifatAlviansyah_3TIP1_JS/tugas1js.js
@@ -0,0 +1,16 @@
+console.log("Ovan Fifat Alviansyah {190100014}");
+console.log("3TI-P1")
+console.log("\n")
+
+firstname = "Ovan" ;
+lastname  = "Alviansyah" ;
+age       = 20 ;
+ismarried = true;
+
+if (ismarried == true){
+    status = "kawin";
+} else {
+    status = "belum kawin"
+}
+
+console.log("Nama saya adalah", firstname , lastname+",", "berumur", age, "tahun", "dengan status perkawinan", status)
---
+++ 19010014_OvanFifatAlviansyah_3TIP1_JS/tugas2js.js
@@ -0,0 +1,18 @@
+console.log("Ovan Fifat Alviansyah {190100014}");
+console.log("3TI-P1")
+console.log("\n")
+
+a = 21;
+b = 7;
+
+console.log("Nilai pada variabel a adalah :", a)
+console.log("Nilai pada variabel b adalah :", b, "\n")
+
+console.log("Nilai dari a == b adalah  :", a==b)
+console.log("Nilai dari a != b adalah  :", a!=b)
+console.log("Nilai dari a === b adalah :", a===b)
+console.log("Nilai dari a !== b adalah :", a!==b)
+console.log("Nilai dari a > b adalah   :", a>b)
+console.log("Nilai dari a < b adalah   :", a<b)
+console.log("Nilai dari a >= b adalah  :", a>=b)
+console.log("Nilai dari a <= b adalah  :", a<=b)
