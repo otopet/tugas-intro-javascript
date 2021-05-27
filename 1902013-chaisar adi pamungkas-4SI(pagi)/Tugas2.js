316284bb57b8d677182ac47e3c895fdd9342d7b2
cesar-2077 <chaisararpansa@gmail.com>
2021-05-27 19:50:55 +0700

    intro java script

---
+++ 1902013-chaisar adi pamungkas-4SI(pagi)/Tugas2.js
@@ -0,0 +1,8 @@
+a = 12;
+b = 6;
+
+console.log("Nilai Variabel a Adalah :", a)
+console.log("Nilai Variabel b Adalah :", b, "\n")
+
+console.log("Nilai dari a == b adalah  :", a==b)
+console.log("Nilai dari a != b adalah  :", a!=b)
---
+++ 1902013-chaisar adi pamungkas-4SI(pagi)/tugas3.js
@@ -0,0 +1,14 @@
+a = 25;
+b = 40;
+
+/* And Operator */
+console.log(a<35 && b>30)
+console.log(a>50 && b<30)
+
+/* OR Operator */
+console.log(a<b || b>a)
+console.log(a<20 || b>35)
+
+/* Not Operator */
+console.log(!a)
+console.log(!b)
