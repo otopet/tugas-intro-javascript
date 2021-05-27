3ed4ad4e894839628e14bd6e4f6d453734fdfbf4
cesar-2077 <chaisararpansa@gmail.com>
2021-05-27 19:29:59 +0700

    first post

---
+++ 19020013-chaisar adi pamungkas-4si(pagi)/tugas1.js
@@ -0,0 +1,12 @@
+
+Nama = Chaisar Adi pamungkas ;
+umur     = 20 ;
+ismarried = true;
+
+if (ismarried == true){
+    status = "kawin";
+} else {
+    status = "belum kawin"
+}
+
+console.log("Nama saya adalah", nama , "berumur", umur, "tahun", "dengan status perkawinan", status)
