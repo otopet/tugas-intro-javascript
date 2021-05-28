bade52fda72f4de1c9fec35b81ce4f1ba6d811f5
putriseptiana <putripadwa16@gmail.com>
2021-05-28 12:41:14 +0700

    119020007_PutriSeptiana_4SIP1_JavaScript

---
+++ 19020007_PutriSeptiana_4SIP1_JavaScript/tugas3.js
@@ -0,0 +1,15 @@
+let a = 8;
+let b = 4;
+
+/* AND operator */
+console.log(a < 20 && b > 2);
+console.log(a > 10 && b < 15);
+
+/* OR operator */
+console.log(a == 8 || b == 4);
+console.log(a == 0 || b == 4);
+
+/* NOT operator */
+console.log(!(a > b));
+console.log(!(a > b && b < a));
+
