74c3ee82075813d0cb04c655979f77b4dd3a158f
Trisna99 <trisnaprwira22@gmail.com>
2021-05-29 15:20:19 +0700

    Tugas

---
+++ 19010008_TrisnaHadiPrawira_4SIP1_JS/tugas3js.js
@@ -0,0 +1,26 @@
+console.log("Trisna Hadi Prawira {190100014}");
+console.log("4SI-P1")
+console.log("\n")
+
+a = 21;
+b = 7;
+
+console.log("Nilai pada variabel a adalah :", a)
+console.log("Nilai pada variabel b adalah :", b, "\n")
+
+// AND operator
+console.log("AND (&&) operator")
+console.log("a < 22 && b > 6 =", a < 22 && b > 6)
+console.log("a < 6 && b >22  =", a < 6 && b >22, "\n")
+
+// OR operator
+console.log("OR (||) operator")
+console.log("a < 23 || b > 8   =", a < 23 || b > 8)
+console.log("a < 22  || b > 21 =", a < 22  || b > 21, "\n")
+
+// NOT operator
+console.log("NOT (!) operator")
+not1 = a > b;
+not2 = b < a;
+console.log("!not1 = a > b =", !not1)
+console.log("!not2 = b < a =",!not2)
