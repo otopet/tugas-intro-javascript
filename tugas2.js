981e9819622af34cc8d6a38de6a925549f32fc67
liaaprita <81305718+liaaprita@users.noreply.github.com>
2021-05-30 22:55:00 +0700

    Update tugas2.js

--- tugas2.js
+++ tugas2.js
@@ -1,9 +1,9 @@
 // gunakan comparison operator
-const nilai1 = ___; // silahkan isi sendiri nilai variabelnya
-const nilai2 = ___;
+const nilai1 = 100; // silahkan isi sendiri nilai variabelnya
+const nilai2 =200;
 
-console.log(___); 
-console.log(___);
+console.log(100); 
+console.log(200);
 
 
 /* output yang dihasilkan harus bernilai boolean sebagai berikut:
