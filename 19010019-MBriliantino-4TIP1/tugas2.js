096717c8db06aff89b042680b1065a5f1b495e72
Briliantino <berlitino@gmail.com>
2021-05-30 23:11:29 +0700

    mengumpulkan tugas

---
+++ 19010019-MBriliantino-4TIP1/tugas2.js
@@ -0,0 +1,12 @@
+a = 11;
+b = 9;
+
+console.log("Nilai variabel a adalah :", a)
+console.log("Nilai variabel b adalah :", b, "\n")
+
+
+console.log("Nilai dari a != b adalah  :", a==b)
+console.log("Nilai dari a === b adalah :", a!=b)
+console.log("Nilai dari a != b adalah  :", a===b)
+console.log("Nilai dari a === b adalah :", a!==b)
+
---
+++ 19010019-MBriliantino-4TIP1/tugas3.js
@@ -0,0 +1,20 @@
+a = 56;
+b = 12;
+
+console.log("Nilai variabel a adalah :", a)
+console.log("Nilai variabel b adalah :", b, "\n")
+
+
+console.log("AND")
+console.log("a < 59 && b > 10 =", a < 59 && b > 10)
+console.log("a < 6 && b >26  =", a < 7 && b >25, "\n")
+
+console.log("OR")
+console.log("a < 70 || b > 9   =", a < 70 || b > 9)
+console.log("a < 60  || b > 3 =", a < 60  || b > 3, "\n")
+
+console.log("NOT")
+not1 = a > b;
+not2 = b < a;
+console.log("!not1 = a > b =", !not1)
+console.log("!not2 = b < a =",!not2)
