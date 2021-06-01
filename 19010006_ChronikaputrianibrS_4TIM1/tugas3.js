3b1882cf22ef703096885bb2cf0c6643f8fc0661
chronikaputriani <chronikapsihombing@gmail.com>
2021-06-01 21:45:20 +0700

    tugas-javascript

---
+++ 19010006_ChronikaputrianibrS_4TIM1/tugas3.js
@@ -0,0 +1,30 @@
+// gunakan logical operator dan comparison operator
+// silahkan isi bebas nilai dari variabel dibawah ini
+
+// Nama : Chronika Putriani br.Sihombing
+// NPM  : 19010006
+
+let a = 9;
+let b = 22;
+
+/* AND operator */
+// angka 15 & 10 merupakan angka bebas, silahkan diisi sesuai kreasi kalian
+console.log(a < 30 && b > 20); // (true && true) -> true
+console.log(a > 30 && b > 20); // (false && true) -> false
+
+/* OR operator */
+console.log(a < 30 || b > 20); // (true || true) -> true
+console.log(a > 30 || b > 20); // (false || true) -> true
+
+/* NOT operator */
+console.log(!(a < 30)); // !(true) -> false
+console.log(!(a < 30 && b > 20)); // !(true && true) -> !(true) -> false
+
+/* output yang dihasilkan
+true
+false
+true
+true
+false
+false
+*/
