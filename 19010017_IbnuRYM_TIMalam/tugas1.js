27c889ac7cd1eb8c842e61b2ded97a1b9477f616
ibnurizki17 <ibnumurdana@gmail.com>
2021-06-07 13:54:25 +0700

    tugas intro javascript

---
+++ 19010017_IbnuRYM_TIMalam/tugas1.js
@@ -0,0 +1,23 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// tulis kode disini tanpa tanda komentar
+
+// Nama : Ibnu Rizki Yurdan Murdana
+// NPM  : 19010017
+
+const firstname = "Ibnu Rizki";
+const lastname = "Yurdan Murdana";
+const age = 24;
+const ismarried = "Belum Kawin";
+
+console.log("Nama saya adalah", firstname, lastname + ",", "berumur", age, "tahun", "dengan status perkawinan", ismarried);
+
+// output yang harus dihasilkan seperti ini:
+// Nama saya adalah <nama_anda>, berumur <umur_anda> tahun dengan status perkawinan <status_anda>.
