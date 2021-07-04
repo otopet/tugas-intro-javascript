685c4f03d78aa6d15cfa7c452d505badf0779de0
NicoArdiansyah <nicoardian94@gmail.com>
2021-07-04 23:11:08 +0700

    tugas intro js

---
+++ Nico Ardiansyah_19010001_4TI Pagi_/Tugas 1.js
@@ -0,0 +1,12 @@
+var firstname = "Nico";
+var lastname = "Ardiansyah";
+var age = 20;
+var isMarried = false;
+if (isMarried == true){
+    status = "kawin";
+} else {
+    status = "belum kawin"
+}
+
+console.log("Nama saya adalah ",firstname,lastname,"berumur",age,"tahun dengan status perkawinan",status);
+
